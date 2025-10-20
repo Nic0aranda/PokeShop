8288c7d1d6b31b9947d3733a7215fa8565161287
Nic0aranda <ale.aranda@duocuc.cl>
2025-10-20 13:34:06 -0300

    entidades y comienzo de Daos

---
+++ app/src/main/java/com/example/pokeshop/data/entities/user.kt
@@ -0,0 +1,27 @@
+package com.example.pokeshop.data.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ForeignKey
+
+
+@Entity(
+    tableName = "users"
+    , foreignKeys =[
+        ForeignKey(
+        entity = RolEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["RolId"],
+            onDelete = ForeignKey.CASCADE
+    )
+    ]
+)
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val names: String,
+    val lastNames: String,
+    val email: String,
+    val password: String,
+    val status: Boolean = true
+)
